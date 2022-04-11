38986964f1f019ebbe211d2f6c96ee5d0775c178
Chief Bird Dog <72176320+Renturly@users.noreply.github.com>
2022-04-11 14:12:57 -0700

    I Love MVVC
    Banking

--- Pending.swift
+++ Pending.swift
@@ -1,8 +1,45 @@
 //
-//  Pending.swift
-//  
+//  ContentView.swift
+//  Pending: Pay is legal
 //
-//  Created by user215493 on 4/5/22.
+//  Created by user215493 on 4/10/22.
 //
 
-import Foundation
+//Hacking Camera is a Motivated Seller vs Military Dog Meat
+import SwiftUI
+
+struct ContentView: View {
+    
+    var guess = 26
+    
+    //Notifcation
+    func killyou(buyer: Int) ->Int {
+        let price = guess - (guess * Int(0.95))
+        return price
+    }
+    
+    var body: some View {
+        Text("Product: Counter Attack! St-54")
+        //House Price is Unpredictable
+        
+        let pending = killyou(buyer: 1200)
+        
+        //guess - (guess * Int(0.95))
+        
+        Text("You won a $\(pending) Offer!")
+            .padding()
+        Text("+60")
+        //Negotiate With Myself till I reach $0.01
+        //$guess is 60 People
+        //The guess is Renturly
+        //Throw A Party
+        //The Product can kill them
+        //Something good will come of the meeting
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
