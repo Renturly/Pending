76e1fb1d2f78c9c6702aa98aa03136545b6af4b5
Chief Bird Dog <72176320+Renturly@users.noreply.github.com>
2022-04-12 17:33:44 -0600

    Accumulate cash in 9 Years

--- Pending.swift
+++ Pending.swift
@@ -1,34 +1,52 @@
 //
 //  ContentView.swift
 //  Pending: Pay is legal
+//  We Need Users is Pull A Card
 //
 //  Created by user215493 on 4/10/22.
 //
 
-//Hacking Camera is a Motivated Seller vs Military Dog Meat
+//Hacking Camera is a Motivated Seller vs Military Dog Meat: I'll get sick if I close this Laptop without the Fan
 import SwiftUI
 
 struct ContentView: View {
     
-    var guess = 26
+    var guess = 100
     
     //Notifcation
     func killyou(buyer: Int) ->Int {
-        let price = guess - (guess * Int(0.95))
-        return price
+        //fistfuls is not serotonin
+        let fistfuls = guess - (guess * Int(0.95))
+        return fistfuls
     }
+    
+    //Limit 30: I Need More Data Is Greed
+    //Uploading Photos is Addictive: Book Value is Accounting: Ben Affleck
+    var funnel = [[1,"Wind",killyou(-1200)],[2,"War",1],[3,"Stock",1],[4,"Prime Minister",1],[4,"Gas",1],[5,"Freedom iPhone 11 Pro Max Chip",1],[6,"Product",1],[7,"$0.50 MINT",4.98],[8,"Product",1],[8,"Product",1],
+        [9,"Product",1],[10,"Product",1],
+        [11,"Product",1],[12,"Product",1],
+        [13,"Product",1],[14,"Product",1],
+        [15,"Product",1],[16,"Product",1],
+        [17,"Product",1],[18,"Product",1],
+        [19,"Product",1],[20,"Product",1],
+        [21,"Product",1],[23,"Product",1],
+        [24,"Product",1],[25,"Product",1],
+        [26,"Product",1],[27,"Product",1],
+        [28,"Product",1],[29,"Product",1],
+        [30,"Product",1]]
+    
+
     
     var body: some View {
         Text("Product: Counter Attack! St-54")
         //House Price is Unpredictable
         
-        let pending = killyou(buyer: 1200)
+        let pending = killyou(buyer: 4.98)
         
         //guess - (guess * Int(0.95))
         
-        Text("You won a $\(pending) Offer!")
+        Text("Book Value $\(pending)")
             .padding()
-        Text("+60")
         //Negotiate With Myself till I reach $0.01
         //$guess is 60 People
         //The guess is Renturly
